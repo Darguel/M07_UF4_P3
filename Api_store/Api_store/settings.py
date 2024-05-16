32b7eb800799744262299d7abd0147e83175f0be
patricia <pvalenci22@jaumebalmes.net>
2024-05-16 16:53:53 +0200

    configuració apps

--- Api_store/Api_store/settings.py
+++ Api_store/Api_store/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'carreto.apps.CarretoConfig',
+    'cataleg.apps.CatalegConfig',
+    'client.apps.ClientConfig',
+    'comandes.apps.ComandesConfig',
+    'pagament.apps.PagamentConfig'
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'Api_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "projectApi",
+        "USER": "user_postgres",
+        "PASSWORD": "pass_postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
